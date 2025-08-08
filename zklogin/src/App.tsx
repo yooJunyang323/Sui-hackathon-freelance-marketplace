e5904194f4968914cafee51d5b6eea3d9044699c
yooju <yoojunyang0323@gmail.com>
2025-08-08 22:57:28 +0800

    Finalise zklogin feature

--- zklogin/src/App.tsx
+++ zklogin/src/App.tsx
@@ -4,16 +4,20 @@ import { createZkLoginSetup, type ZkLoginSetup } from './lib/ephemeralKey'; // I
 import { GoogleJwtLogin } from './lib/auth/generateJWT'; 
 import './App.css';
 import { generateProof } from './lib/auth/createZkProof';
+import { ConnectButton } from "@suiet/wallet-kit";
+import { useWallet } from "@suiet/wallet-kit";
 
 
 function App() {
   const [setupData, setSetupData] = useState<ZkLoginSetup | null>(null);
   const [jwt, setJwt] = useState<string | null>(null);
   const [suiAddress, setSuiAddress] = useState<string | null>(null);
+  const wallet = useWallet();
 
 
-  const connectWallet = () => {
-    // ...
+
+  const connectWallet = async () => {
+    ///
   };
 
   const loginWithGoogle = async () => {
@@ -30,7 +34,7 @@ function App() {
   const handleJwt = async (jwt: string) => {
     console.log("JWT received:", jwt);
     setJwt(jwt);
-    
+  
     if (setupData) {
       try {
         const { proofPoints, userSuiAddress, salt, decodedPayload } = await generateProof(jwt, setupData);
@@ -41,18 +45,32 @@ function App() {
         console.error("Failed to generate proof or get Sui address:", error);
       }
     }
-     
-    
+  
+  
   };
 
   return (
     <>
     <h1>Welcome to Sui Zklogin</h1>
 
-    <button className="google-btn" onClick={connectWallet}>
-      🔐 Start Ewallet Login
-    </button>
-
+    <ConnectButton 
+      className='btn'
+      label="🔐 Start Ewallet Login"
+      style={{  
+        color: 'rgb(240, 240, 240)', 
+        backgroundColor: '#1A1A1A', 
+        padding: '0.6em 1.2em',
+        borderRadius: '8px',
+        border: '1px solid transparent',
+        fontSize: '1em',
+        fontWeight: 500,
+        fontFamily: 'inherit',
+        cursor: 'pointer',
+        boxShadow: 'rgba(0, 0, 0, 0.1) 0px 4px 6px',
+        transition: 'border-color 0.25s',
+      }}/>
+    
+    
     <button className="google-btn" onClick={loginWithGoogle}>
       🌐 Start Google Login
     </button>
@@ -68,6 +86,14 @@ function App() {
       <div style={{ marginTop: '20px' }}>
         <p>JWT received!</p>
       </div>
+    )}
+
+    
+    {wallet.connected && wallet.account && (
+        <div style={{ marginTop: '20px' }}>
+            <p>Wallet connected!</p>
+            <p>Wallet Address: {wallet.account.address}</p>
+        </div>
     )}
     </>
   );
--- zklogin/src/main.tsx
+++ zklogin/src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { WalletProvider } from "@suiet/wallet-kit";
+import "@suiet/wallet-kit/style.css";
 
 const GOOGLE_CLIENT_ID = '932006901435-miijfmeofbbgs07difqp56lpj8406jj7.apps.googleusercontent.com';
 
@@ -10,7 +12,9 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
       {/* <App /> is now a child of GoogleOAuthProvider */}
-      <App />
+      <WalletProvider>
+        <App />
+      </WalletProvider>
     </GoogleOAuthProvider>
   </StrictMode>,
 );
