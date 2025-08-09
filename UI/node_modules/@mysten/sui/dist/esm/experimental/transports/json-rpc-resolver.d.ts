dd4d2fafb3e18e0874974857b01ff4e2bc804956
RJtheDJ <41671288+RJtheDJ@users.noreply.github.com>
2025-08-09 15:26:45 +0800

    update sui

--- UI/node_modules/@mysten/sui/dist/esm/experimental/transports/json-rpc-resolver.d.ts
+++
@@ -1,4 +0,0 @@
-import type { TransactionDataBuilder } from '../../transactions/TransactionData.js';
-import type { SuiClient } from '../../client/index.js';
-import type { BuildTransactionOptions } from '../../transactions/index.js';
-export declare function suiClientResolveTransactionPlugin(client: SuiClient): (transactionData: TransactionDataBuilder, options: BuildTransactionOptions, next: () => Promise<void>) => Promise<void>;
--- UI/node_modules/@mysten/utils/dist/esm/b64.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function fromBase64(base64String: string): Uint8Array;
-export declare function toBase64(bytes: Uint8Array): string;
